9cc97da1e5fedeae28ae6e16b19b99d46c092c46
Zombee0804 <samb8424@gmail.com>
2021-10-18 22:10:16 +0100

    Added 'platformerTest' level
    Added a platformer test level. Includes basic player movement, as well as a jump boost, speed boost, and camera movement.

---
+++ systemTesting/Assets/platformerTest/Scripts/cameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float movementDiv;
+
+    void FixedUpdate() {
+        if (target != null) {
+            Vector3 movement = target.position - transform.position;
+            movement.z = 0;
+            transform.position += movement/movementDiv;
+        }
+    }
+}
