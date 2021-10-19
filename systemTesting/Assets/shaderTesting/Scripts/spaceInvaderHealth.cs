26015a44ac77b5224ab3a9b1c83ee656621cc3b8
Zombee0804 <samb8424@gmail.com>
2021-10-19 17:26:37 +0100

    Added Shaders
    Added a dissolve shader and hit shader to the player character.
    
    Minor tweaks to gameplay.

--- systemTesting/Assets/shaderTesting/Scripts/spaceInvaderHealth.cs
+++ systemTesting/Assets/shaderTesting/Scripts/spaceInvaderHealth.cs
@@ -8,18 +8,50 @@ public class spaceInvaderHealth : MonoBehaviour
     private float currentHealth;
     public bool isAlive;
 
+    [Header("Shader Variables")]
+    public Material mat;
+    private float fadeValue;
+    public float fadeRate;
+
+    public float hitLength;
+    private float hitAlarm;
+
     void Start() {
         currentHealth = startingHealth;
         isAlive = true;
+
+        mat = GetComponent<SpriteRenderer>().material;
+        fadeValue = 1;
     }
 
     void Update() {
-        if (isAlive == false) Destroy(gameObject);
+        if (isAlive == false) {
+            if (fadeValue > 0) {
+                fadeValue -= fadeRate * Time.deltaTime;
+                mat.SetFloat("_StepValue", fadeValue);
+            }
+            else {
+                Destroy(gameObject);
+            }
+        }
+
+        if (mat.GetInt("_IsHit") == 1) {
+            hitAlarm += Time.deltaTime;
+            if (hitAlarm > hitLength) {
+                mat.SetInt("_IsHit", 0);
+            }
+        }
     }
 
     void Hit() {
         if (currentHealth > 0) currentHealth -= 1;
-        if (currentHealth <= 0) isAlive = false;
+        if (currentHealth <= 0) {
+            isAlive = false;
+        }
+        else {
+            mat.SetInt("_IsHit", 1);
+            hitAlarm = 0;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col) {
--- systemTesting/Assets/shaderTesting/Scripts/spaceInvaderMovement.cs
+++ systemTesting/Assets/shaderTesting/Scripts/spaceInvaderMovement.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class spaceInvaderMovement : MonoBehaviour
 {
     public Rigidbody2D playerBody;
+    public spaceInvaderHealth SIHealth;
     public float moveSpeed;
 
     void Update() {
-        float horizontalMovement = Input.GetAxisRaw("Horizontal");
-        float verticalMovement = Input.GetAxisRaw("Vertical");
-        Vector2 movement = new Vector2(horizontalMovement, verticalMovement);
+        if (SIHealth.isAlive == true) {
+            float horizontalMovement = Input.GetAxisRaw("Horizontal");
+            float verticalMovement = Input.GetAxisRaw("Vertical");
+            Vector2 movement = new Vector2(horizontalMovement, verticalMovement);
 
-        playerBody.position += movement * moveSpeed * Time.deltaTime;
+            playerBody.position += movement * moveSpeed * Time.deltaTime;
+        }
     }
 }
