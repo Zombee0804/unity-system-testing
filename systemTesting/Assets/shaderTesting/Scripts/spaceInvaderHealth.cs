5981031c88c411dcaf616a4340babd0e15cf649b
Zombee0804 <samb8424@gmail.com>
2021-10-19 16:11:19 +0100

    Setup 'shaderTest' level
    Added a new level called 'shaderTest' and setup up the basic gameplay of a space invader character and randomly generated projectiles

---
+++ systemTesting/Assets/shaderTesting/Scripts/spaceInvaderHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceInvaderHealth : MonoBehaviour
+{
+    public float startingHealth;
+    private float currentHealth;
+    public bool isAlive;
+
+    void Start() {
+        currentHealth = startingHealth;
+        isAlive = true;
+    }
+
+    void Update() {
+        if (isAlive == false) Destroy(gameObject);
+    }
+
+    void Hit() {
+        if (currentHealth > 0) currentHealth -= 1;
+        if (currentHealth <= 0) isAlive = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "bullet") {
+            Destroy(col.gameObject);
+            Hit();
+        }
+    }
+}
---
+++ systemTesting/Assets/shaderTesting/Scripts/spaceInvaderMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceInvaderMovement : MonoBehaviour
+{
+    public Rigidbody2D playerBody;
+    public float moveSpeed;
+
+    void Update() {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(horizontalMovement, verticalMovement);
+
+        playerBody.position += movement * moveSpeed * Time.deltaTime;
+    }
+}
