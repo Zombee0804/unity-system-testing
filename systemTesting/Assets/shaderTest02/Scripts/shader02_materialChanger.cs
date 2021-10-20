ad328224a11066bf4aca66a5c54ea8e99ea69263
Zombee0804 <samb8424@gmail.com>
2021-10-20 14:11:04 +0100

    Added Shaders to 'shaderTest02'
    The movement shifting works, the colour shifting doesn't

---
+++ systemTesting/Assets/shaderTest02/Scripts/shader02_materialChanger.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shader02_materialChanger : MonoBehaviour
+{
+
+    // _VoronoiAngle, _VoronoiX, _VoronoiY, _VoronoiColour
+
+    public Material mat;
+
+    [Header("Position Variables")]
+    public float posChangeAmount;
+    public float posFadeAmount;
+
+    public float posChangeLengthMin;
+    public float posChangeLengthMax;
+
+    public float posChangeLengthX;
+    public float posChangeLengthY;
+
+    public float posChangeAlarmX;
+    public float posChangeAlarmY;
+
+    public float posIncreasingX;
+    public float posIncreasingY;
+
+    public float posIncreasingXTarget;
+    public float posIncreasingYTarget;
+
+    [Header("Angle Variables")]
+    public float angChangeAmount;
+    public float angFadeAmount;
+
+    public float angChangeLengthMin;
+    public float angChangeLengthMax;
+
+    public float angChangeLength;
+    public float angChangeAlarm;
+    public float angIncreasing;
+    public float angIncreasingTarget;
+
+    [Header("Colour Variables")]
+    public bool empty;
+
+    void Start() {
+        mat = GetComponent<SpriteRenderer>().material;
+        posIncreasingX = 1;
+        posIncreasingY = 1;
+        posIncreasingXTarget = -1;
+        posIncreasingYTarget = -1;
+
+        angIncreasing = 1;
+        angIncreasingTarget = -1;
+    }
+
+    void Update() {
+        PosChange();
+        AngleChange();
+        ColourChange();
+    }
+
+    void PosChange() {
+        float voroX = mat.GetFloat("_VoronoiX");
+
+        voroX += posChangeAmount * posIncreasingX * Time.deltaTime;
+        if (posIncreasingXTarget == -1) {
+            posChangeAlarmX += Time.deltaTime;
+            if (posChangeAlarmX >= posChangeLengthX) {
+                posChangeAlarmX = 0;
+                posChangeLengthX = Random.Range(posChangeLengthMin, posChangeLengthMax);
+                posIncreasingXTarget = Random.Range(0, 0 - Mathf.Sign(posIncreasingX));
+            }
+        }
+        else {
+            if (posIncreasingXTarget < 0) {
+                posIncreasingX -= posFadeAmount * Time.deltaTime;
+                if (posIncreasingX <= posIncreasingXTarget) {
+                    posIncreasingXTarget = -1;
+                }
+            }
+            else if (posIncreasingXTarget > 0) {
+                posIncreasingX += posFadeAmount * Time.deltaTime;
+                if (posIncreasingX >= posIncreasingXTarget) {
+                    posIncreasingXTarget = -1;
+                }
+            }
+        }
+
+        mat.SetFloat("_VoronoiX", voroX);
+        
+        float voroY = mat.GetFloat("_VoronoiY");
+
+        voroY += posChangeAmount * posIncreasingY * Time.deltaTime;
+        if (posIncreasingYTarget == -1) {
+            posChangeAlarmY += Time.deltaTime;
+            if (posChangeAlarmY >= posChangeLengthY) {
+                posChangeAlarmY = 0;
+                posChangeLengthY = Random.Range(posChangeLengthMin, posChangeLengthMax);
+                posIncreasingYTarget = Random.Range(0, 0 - Mathf.Sign(posIncreasingY));
+            }
+        }
+        else {
+            if (posIncreasingYTarget < 0) {
+                posIncreasingY -= posFadeAmount * Time.deltaTime;
+                if (posIncreasingY <= posIncreasingYTarget) {
+                    posIncreasingYTarget = -1;
+                }
+            }
+            else if (posIncreasingYTarget > 0) {
+                posIncreasingY += posFadeAmount * Time.deltaTime;
+                if (posIncreasingY >= posIncreasingYTarget) {
+                    posIncreasingYTarget = -1;
+                }
+            }
+        }
+
+        mat.SetFloat("_VoronoiY", voroY);
+    }
+
+    void AngleChange() {
+        float ang = mat.GetFloat("_VoronoiAngle");
+
+        ang += angChangeAmount * angIncreasing * Time.deltaTime;
+        if (angIncreasingTarget == -1) {
+            angChangeAlarm += Time.deltaTime;
+            if (angChangeAlarm >= angChangeLength) {
+                angChangeAlarm = 0;
+                angChangeLength = Random.Range(angChangeLengthMin, angChangeLengthMax);
+                angIncreasingTarget = Random.Range(0, 0 - Mathf.Sign(angIncreasing));
+            }
+        }
+        else {
+            if (angIncreasingTarget < 0) {
+                angIncreasing -= angFadeAmount * Time.deltaTime;
+                if (angIncreasing <= angIncreasingTarget) {
+                    angIncreasingTarget = -1;
+                }
+            }
+            else if (angIncreasingTarget > 0) {
+                angIncreasing += angFadeAmount * Time.deltaTime;
+                if (angIncreasing >= angIncreasingTarget) {
+                    angIncreasingTarget = -1;
+                }
+            }
+        }
+
+        mat.SetFloat("_VoronoiAngle", ang);
+    }
+
+    void ColourChange() {
+
+    }
+}
---
+++ systemTesting/Assets/shaderTest02/Scripts/shader02_valueChanger.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shader02_valueChanger : MonoBehaviour
+{
+
+    // changeAmount, fadeAmount, lengthMin, lengthMax, length, alarm, increasing, increasingTarget
+
+    public Material mat;
+
+    [Header("Position X Variables")]
+    public string xReference;
+    public float xChangeAmount;
+    public float xFadeAmount;
+    public float xLengthMin;
+    public float xLengthMax;
+    public float xLength;
+    public float xAlarm;
+    public float xIncreasing;
+    public float xIncreasingTarget;
+    public Dictionary<string, float> posXValues= new Dictionary<string, float>();
+
+    [Header("Position Y Variables")]
+    public string yReference;
+    public float yChangeAmount;
+    public float yFadeAmount;
+    public float yLengthMin;
+    public float yLengthMax;
+    public float yLength;
+    public float yAlarm;
+    public float yIncreasing;
+    public float yIncreasingTarget;
+    public Dictionary<string, float> posYValues= new Dictionary<string, float>();
+
+    [Header("Angle Variables")]
+    public string angleReference;
+    public float angleChangeAmount;
+    public float angleFadeAmount;
+    public float angleLengthMin;
+    public float angleLengthMax;
+    public float angleLength;
+    public float angleAlarm;
+    public float angleIncreasing;
+    public float angleIncreasingTarget;
+    public Dictionary<string, float> angleValues= new Dictionary<string, float>();
+
+    [Header("Colour Variables")]
+    public float colourChangeAmount;
+    public float colourFadeAmount;
+    public float colourLengthMin;
+    public float colourLengthMax;
+    public float colourLength;
+    public float colourAlarm;
+    public float colourIncreasing;
+    public float colourIncreasingTarget;
+    public Dictionary<string, float> colourRed = new Dictionary<string, float>();
+    public Dictionary<string, float> colourGreen = new Dictionary<string, float>();
+    public Dictionary<string, float> colourBlue = new Dictionary<string, float>();
+
+    void Start() {
+        mat = GetComponent<SpriteRenderer>().material;
+
+        posXValues.Add("changeAmount", xChangeAmount);
+        posXValues.Add("fadeAmount", xFadeAmount);
+        posXValues.Add("lengthMin", xLengthMin);
+        posXValues.Add("lengthMax", xLengthMax);
+        posXValues.Add("length", xLength);
+        posXValues.Add("alarm", xAlarm);
+        posXValues.Add("increasing", xIncreasing);
+        posXValues.Add("increasingTarget", xIncreasingTarget);
+
+        posYValues.Add("changeAmount", yChangeAmount);
+        posYValues.Add("fadeAmount", yFadeAmount);
+        posYValues.Add("lengthMin", yLengthMin);
+        posYValues.Add("lengthMax", yLengthMax);
+        posYValues.Add("length", yLength);
+        posYValues.Add("alarm", yAlarm);
+        posYValues.Add("increasing", yIncreasing);
+        posYValues.Add("increasingTarget", yIncreasingTarget);
+
+        angleValues.Add("changeAmount", angleChangeAmount);
+        angleValues.Add("fadeAmount", angleFadeAmount);
+        angleValues.Add("lengthMin", angleLengthMin);
+        angleValues.Add("lengthMax", angleLengthMax);
+        angleValues.Add("length", angleLength);
+        angleValues.Add("alarm", angleAlarm);
+        angleValues.Add("increasing", angleIncreasing);
+        angleValues.Add("increasingTarget", angleIncreasingTarget);
+
+        Dictionary<string, float> colourValues = new Dictionary<string, float>();
+        colourValues.Add("changeAmount", colourChangeAmount);
+        colourValues.Add("fadeAmount", colourFadeAmount);
+        colourValues.Add("lengthMin", colourLengthMin);
+        colourValues.Add("lengthMax", colourLengthMax);
+        colourValues.Add("length", colourLength);
+        colourValues.Add("alarm", colourAlarm);
+        colourValues.Add("increasing", colourIncreasing);
+        colourValues.Add("increasingTarget", colourIncreasingTarget);
+
+        colourRed = colourValues;
+        colourGreen = colourValues;
+        colourBlue = colourValues;
+    }
+
+    void Update() {
+        posXValues = FadeValue(xReference, posXValues);
+        posYValues = FadeValue(yReference, posYValues);
+        angleValues = FadeValue(angleReference, angleValues);
+
+        colourRed = FadeValue("_VoroR", colourRed, true);
+        colourGreen = FadeValue("_VoroG", colourGreen, true);
+        colourBlue = FadeValue("_VoroB", colourBlue, true);
+    }
+
+    Dictionary<string, float> FadeValue(string shaderRef, Dictionary<string, float> dict, bool mod = false) {
+
+        float currentVal = mat.GetFloat(shaderRef);
+        Debug.Log(shaderRef + currentVal.ToString());
+
+        currentVal += dict["changeAmount"] * dict["increasing"] * Time.deltaTime;
+        if (dict["increasingTarget"] == -1) {
+            dict["alarm"] += Time.deltaTime;
+            if (dict["alarm"] >= dict["length"]) {
+                dict["alarm"] = 0;
+                dict["length"] = Random.Range(dict["lengthMin"], dict["lengthMax"]);
+                dict["increasingTarget"] = Random.Range(0, 0 - Mathf.Sign(dict["increasing"]));
+            }
+        }
+        else {
+            if (dict["increasingTarget"] < 0) {
+                dict["increasing"] -= dict["fadeAmount"] * Time.deltaTime;
+                if (dict["increasing"] <= dict["increasingTarget"]) {
+                    dict["increasingTarget"] = -1;
+                }
+            }
+            else if (dict["increasingTarget"] > 0) {
+                dict["increasing"] += dict["fadeAmount"] * Time.deltaTime;
+                if (dict["increasing"] >= dict["increasingTarget"]) {
+                    dict["increasingTarget"] = -1;
+                }
+            }
+        }
+
+        if (mod == true) {
+            currentVal = currentVal % 255;
+        }
+        mat.SetFloat(shaderRef, currentVal);
+        return dict;
+    }
+}
