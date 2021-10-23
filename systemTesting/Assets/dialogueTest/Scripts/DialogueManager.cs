b7dc913d802828b5db77473334df03d200636f6f
Zombee0804 <samb8424@gmail.com>
2021-10-23 15:11:43 +0100

    Added Response UI

--- systemTesting/Assets/dialogueTest/Scripts/DialogueManager.cs
+++ systemTesting/Assets/dialogueTest/Scripts/DialogueManager.cs
@@ -13,8 +13,23 @@ public class DialogueManager : MonoBehaviour
     private int sentenceIndex;
     private bool displayedCurrent;
 
-    [Header("Resopnse Variables")]
+    [Header("Resopnse Logic Variables")]
     public bool displayedResponses;
+    public int currentlySelected;
+
+    [Header("Response UI Variables")]
+    public float responseYPos;
+    public float screenWidth;
+    public float[] response2Pos;
+    public float[] response3Pos;
+
+    [Header("UI Settings")]
+    public Text speakerText;
+    public Text dialogueText;
+    public Font dialogueFont;
+    public Transform dialogueCanvas;
+    public SpriteRenderer dialogueBox;
+    private List<Text> UIResponses;
 
     [Header("Other")]
     public int dialogueIndex;
@@ -26,11 +41,6 @@ public class DialogueManager : MonoBehaviour
         waiting
     }
     public DIALOGUE_STATE currentState;
-
-    [Header("UI Settings")]
-    public SpriteRenderer dialogueBox;
-    public Text speakerText;
-    public Text dialogueText;
 
     void Start() {
         currentDialogue = startingDialogue;
@@ -100,59 +110,117 @@ public class DialogueManager : MonoBehaviour
 
     void StateWaiting() {
         ResponseObject[] responses = currentDialogue.responses;
+
         if (displayedResponses == false) {
-            for (var i = 0; i < responses.Length; i += 1) {
-                var displayIndex = i + 1;
-                DisplayResposne(displayIndex.ToString(), responses[i].responseText);
-            }
+            UIResponses = DisplayResponses(responses);
             displayedResponses = true;
         }
         else {
-            int inputChoice = 0;
+            int confirmedChoice = 0;
             if (Input.anyKey) {
                 for (var i = 0; i < Input.inputString.Length; i += 1) {
                     char currentKey = Input.inputString[i];
                     if (Char.IsDigit(currentKey) == true) {
                         int keyInt = int.Parse(currentKey.ToString());
-                        if (keyInt <= responses.Length) {
-                            inputChoice = keyInt;
+                        if (keyInt <= UIResponses.Count) {
+                            currentlySelected = keyInt;
+                            for (var x = 0; x < UIResponses.Count; x += 1) {
+                                if (x == currentlySelected-1) {
+                                    UIResponses[x].color = Color.yellow;
+                                }
+                                else {
+                                    UIResponses[x].color = Color.white;
+                                }
+                            }
                             break;
+                        }
+                        else {
+                            Debug.Log("Invalid Selection");
+                            Debug.Log(UIResponses.Count);
                         }
                     }
                 }
             }
 
-            if (inputChoice != 0) {
-                currentDialogue = responses[inputChoice-1].nextDialogue;
+            if (Input.GetKeyDown(KeyCode.Return) == true && confirmedChoice == 0) {
+                confirmedChoice = currentlySelected;
+            }
+
+            if (confirmedChoice != 0) {
+                currentDialogue = responses[confirmedChoice-1].nextDialogue;
                 currentState = DIALOGUE_STATE.displaying;
                 sentenceIndex = 0;
                 displayedCurrent = false;
+                ClearResponses();
             }
         }
     }
 
     void DisplaySentence(string speaker, string sentence) {
         dialogueBox.enabled = true;
-        string displayText = "";
-        for (var i = 0; i < sentence.Length; i += 1) {
-            if (sentence[i] + "" == " ") {
-                displayText += "     ";
-            }
-            else {
-                displayText += sentence[i];
-            }
-        }
         speakerText.text = speaker;
-        dialogueText.text = displayText;
+        dialogueText.text = ExpandSpaces(sentence);
     }
 
-    void DisplayResposne(string inputKey, string sentence) {
-        dialogueBox.enabled = true;
-        Debug.Log(" RESPONSE: " + inputKey + " - " + sentence);
+    List<Text> DisplayResponses(ResponseObject[] responses) {
+        List<Text> UIResponses = new List<Text>();
+        for (var i = 0; i < responses.Length; i += 1) {
+            float xPos = 0;
+            if (responses.Length == 2) {
+                xPos = response2Pos[i];
+            }
+            else if (responses.Length == 3) {
+                xPos = response3Pos[i];
+            }
+            Text r = CreateText("response" + (i+1).ToString(), dialogueCanvas, xPos, responseYPos, ExpandSpaces(responses[i].responseText));
+            UIResponses.Add(r);
+        }
+
+        return UIResponses;
     }
 
     void ClearDialogueBox() {
         DisplaySentence(" ", " ");
         dialogueBox.enabled = false;
+    }
+
+    void ClearResponses() {
+        foreach (Text t in UIResponses) {
+            Destroy(t.gameObject);
+        }
+    }
+
+    string ExpandSpaces(string text) {
+        string newText = "";
+        for (var i = 0; i < text.Length; i += 1) {
+            if (text[i] + "" == " ") {
+                newText += "     ";
+            }
+            else {
+                newText += text[i];
+            }
+        }
+
+        return newText;
+    }
+
+    Text CreateText(string objectName, Transform canvas, float x, float y, string displayText)
+    {
+        GameObject textObject = new GameObject(objectName);
+        textObject.transform.SetParent(canvas);
+
+        RectTransform trans = textObject.AddComponent<RectTransform>();
+        trans.sizeDelta = new Vector2(250, 30);
+        trans.localScale = new Vector3(1, 1, 1);
+        trans.anchoredPosition3D = new Vector3(x, y, 0);
+
+        Text text = textObject.AddComponent<Text>();
+        text.text = displayText;
+        text.font = dialogueFont;
+        text.fontSize = 24;
+        text.color = Color.white;
+        text.alignment = TextAnchor.MiddleCenter;
+
+        return text;
     }
 }
