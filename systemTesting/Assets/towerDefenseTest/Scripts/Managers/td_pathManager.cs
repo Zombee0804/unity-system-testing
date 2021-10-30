71cbb84b395f18a309d31c1bed3a011f721b49a2
Zombee0804 <samb8424@gmail.com>
2021-10-30 16:10:22 +0100

    Added path visualisation (closes #13)
    A sprite is now generated between every pair of path corners and stretched the fill the gap.

--- systemTesting/Assets/towerDefenseTest/Scripts/Managers/td_pathManager.cs
+++ systemTesting/Assets/towerDefenseTest/Scripts/Managers/td_pathManager.cs
@@ -5,6 +5,29 @@ using UnityEngine;
 public class td_pathManager : MonoBehaviour
 {
     public GameObject[] pathCorners;
+    
+    [Header("Visual Vars")]
+    public GameObject pathPrefab;
+    public float pathVisualWidth;
+
+    void Start() {
+        for (var i = 0; i < pathCorners.Length-1; i += 1) {
+            Vector3 spawnPos = (pathCorners[i].transform.position + pathCorners[i+1].transform.position) / 2;
+            Vector3 scaleVec = pathCorners[i].transform.position - pathCorners[i+1].transform.position;
+            
+            if (scaleVec.x < 0.1f) {
+                scaleVec.x = pathVisualWidth;
+                scaleVec.y += pathVisualWidth;
+            }
+            else if (scaleVec.y < 0.1f) {
+                scaleVec.y = pathVisualWidth;
+                scaleVec.x += pathVisualWidth;
+            }
+
+            GameObject path = Instantiate(pathPrefab, spawnPos, Quaternion.identity);
+            path.transform.localScale = scaleVec;
+        }
+    }
 
     void OnDrawGizmos() {
         Gizmos.color = Color.blue;
