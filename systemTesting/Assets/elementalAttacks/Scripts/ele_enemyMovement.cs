e5099ecb36e17c9872d080aff754fa94db250ecf
Zombee0804 <samb8424@gmail.com>
2021-11-17 20:06:08 +0000

    Added player movement and enemies

---
+++ systemTesting/Assets/elementalAttacks/Scripts/ele_enemyMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ele_enemyMovement : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody2D enemyBody;
+    public Rigidbody2D playerBody;
+    public ele_playerMovement playerMovement;
+
+    [Header("Movement")]
+    public float moveSpeed;
+
+    [Header("Health")]
+    public float startingHealth;
+    public float currentHealth;
+
+    [Header("Attacking")]
+    private float distanceFromPlayer;
+    private bool playerGrounded;
+    public float attackRange;
+
+    public float damage;
+    public float damageBuffer;
+    private float damageAlarm;
+
+    void Start() {
+        currentHealth = startingHealth;
+    }
+
+    void Update() {
+        if (playerBody != null) {
+            distanceFromPlayer = (playerBody.position - enemyBody.position).x;
+            playerGrounded = playerMovement.isGrounded;
+            EnemyMovement();
+            EnemyHealth();
+            EnemyAttacking();
+        }
+    }
+
+    void EnemyMovement() {
+        RaycastHit2D sight = Physics2D.Raycast(transform.position, new Vector2(Mathf.Sign(distanceFromPlayer), 0), 0.6f);
+        if (sight.collider == null) {
+            enemyBody.position += new Vector2(Mathf.Sign(distanceFromPlayer), 0) * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    void EnemyHealth() {
+        if (currentHealth <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    void EnemyAttacking() {
+        if (distanceFromPlayer < attackRange && playerGrounded) {
+            if (damageAlarm <= 0) {
+                playerMovement.currentHealth -= damage;
+                damageAlarm = damageBuffer;
+            }
+            else {
+                damageAlarm -= Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ systemTesting/Assets/elementalAttacks/Scripts/ele_playerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ele_playerMovement : MonoBehaviour
+{
+    [Header("References")]
+    public Rigidbody2D playerBody;
+    public SpriteRenderer playerRen;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float jumpForce;
+    private int jumpCount;
+    public int jumpCountMax;
+    public bool isGrounded;
+
+    [Header("Health")]
+    public float startingHealth;
+    public float currentHealth;
+
+    void Start() {
+        currentHealth = startingHealth;
+    }
+
+    void Update() {
+        PlayerMovement();
+        PlayerHealth();
+        PlayerColourChange();
+    }
+
+    void PlayerMovement() {
+        float movement = Input.GetAxisRaw("Horizontal");
+        playerBody.position += new Vector2(movement, 0) * moveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < jumpCountMax) {
+            playerBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpCount += 1;
+            isGrounded = false;
+        }
+    }
+
+    void PlayerHealth() {
+        if (currentHealth <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    void PlayerColourChange() {
+        Color toSet = Color.white;
+        toSet.g = (currentHealth/startingHealth);
+        toSet.b = (currentHealth/startingHealth);
+        playerRen.color = toSet;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "ground_normal") {
+            jumpCount = 0;
+            isGrounded = true;
+        }
+    }
+}
